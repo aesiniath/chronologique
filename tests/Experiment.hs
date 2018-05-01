2708a4ce7977486b29d8457db2d6e0a66db61bd5
Andrew Cowie <andrew@operationaldynamics.com>
2018-05-01 22:54:12 +1000

    Test case for encoding to and decoding from JSON

--- tests/Experiment.hs
+++ tests/Experiment.hs
@@ -4,8 +4,26 @@
 module Main where
 
 import Chrono.TimeStamp
+import Data.Aeson
+import Data.ByteString.Builder
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.Text.IO as T
+import System.IO
+
+x :: L.ByteString
+x = "2018-05-01T01:42:12Z"
+t = read (L.unpack x) :: TimeStamp
+j = toEncoding t
 
 main :: IO ()
 main = do
-    putStrLn "Hello"
- 
+    putStrLn . show . unTimeStamp $ t
+    putStrLn . show . toJSON $ t
+    L.putStrLn . toLazyByteString . fromEncoding . toEncoding $ t
+    L.putStrLn . encode $ t
+
+    putStrLn . show . (decode :: L.ByteString -> Maybe TimeStamp) . encode $ t
+    putStrLn . show . (eitherDecode :: L.ByteString -> Either String TimeStamp) $ "234"
+    putStrLn . show . (eitherDecode :: L.ByteString -> Either String TimeStamp) $ L.concat ["\"", x, "\""]
+    putStrLn . show . (eitherDecode :: L.ByteString -> Either String TimeStamp) $ L.concat ["\"", "2018-05:01T01:42:12Z" , "\""]
+
